9d9dac7715d63f84dfa1d7c308bcc23319e02180
pablo.ruhl <pabloalejandroruhl@gmail.com>
2024-01-31 09:50:22 -0300

    User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Pablo',
+            'email' => 'pablo+1@gmail.com',
+        ]);
+    }
+}
